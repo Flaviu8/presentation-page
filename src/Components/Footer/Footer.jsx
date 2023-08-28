966a8c3df3d7862cd1f4f0e730caa91dd944fd29
Flaviu10 <flavius_flaviu93@yahoo.com>
2023-08-28 17:31:48 +0100

    resize footer font size

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
         <p className='text-3xl' >Thanks for watching my page!</p>
         <p className='text-1xl mt-2' >Download my resume from button bellow</p>
         <a className=" flex w-36 bg-indigo-500 hover:bg-indigo-600 text-white py-2 px-4 rounded mt-4" href="/assets/pdf/Ioan-Flaviu-Resume.pdf" target="_blank" download >
-            <p className='text-sm'>Download Cv</p><img src={download}></img></a>
+            <p className='download_btn'>Download Cv</p><img src={download}></img></a>
         </div>
           <div className='air air1'></div>
           <div className='air air2'></div>
