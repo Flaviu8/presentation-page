502a8b222d19f18bb7de23505c999864b316a1cf
Flaviu10 <flavius_flaviu93@yahoo.com>
2023-08-28 17:28:14 +0100

    resize photo

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,9 +5,9 @@ import download from '../../assets/icons/Download.svg'
 export default function Footer() {
   return (
     <section className='bg-gradient-to-r from-violet-300 to-indigo-600 w-full mt-4 h-[26rem] relative'>
-        <div className=' flex flex-col items-center	mt-36 banner'>
-        <p className='text-4xl mt-4' >Thanks for watching my page!</p>
-        <p className='text-1xl mt-4' >Download my resume from button bellow</p>
+        <div className=' flex flex-col items-center	mt-16 banner'>
+        <p className='text-4xl' >Thanks for watching my page!</p>
+        <p className='text-1xl mt-2' >Download my resume from button bellow</p>
         <a className=" flex w-36 bg-indigo-500 hover:bg-indigo-600 text-white py-2 px-4 rounded mt-4" href="/assets/pdf/Ioan-Flaviu-Resume.pdf" target="_blank" download >
             <p className='text-sm'>Download Cv</p><img src={download}></img></a>
         </div>
