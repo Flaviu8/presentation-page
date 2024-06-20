93a22e6cd12bdc0bc9d70245e03722706969e31f
Flaviu <98925455+Flaviu8@users.noreply.github.com>
2024-06-20 23:44:30 +0100

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <div className=' flex flex-col items-center	mt-16 banner'>
         <p className='text-3xl' >Thanks for watching my page!</p>
         <p className='text-1xl mt-2' >Download my resume from button bellow</p>
-        <a className=" flex w-36 bg-indigo-500 hover:bg-indigo-600 text-white py-2 px-4 rounded mt-4" href="/assets/pdf/Ioan-Flaviu-Resume.pdf" target="_blank" download >
+        <a className=" flex w-36 bg-indigo-500 hover:bg-indigo-600 text-white py-2 px-4 rounded mt-4" href="/assets/pdf/Ioan-Flaviu-Resume.pdf" target="_blank" download="Ioan-Flaviu-Resume.pdf" >
             <p className='download_btn'>Download</p><img src={download}></img></a>
         </div>
           <div className='air air1'></div>
